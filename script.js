a14f7ced85dd71bf79fd5e6047aaec40458a55fc
José Jesus <104231109+JoseJesusNetero@users.noreply.github.com>
2026-02-12 02:01:23 -0300

    fdgedrggrd

---
+++ script.js
@@ -0,0 +1,32 @@
+const Pessoa( id, nome, cidade, idade, interesse, imagem ) {
+
+
+        this.id = id;
+        this.nome  = nome;
+        this.idade = idade;
+        this.posicao = posicao;
+
+        
+
+        Pessoa.propotype = 
+
+         Pessoa = new Pessoa();
+
+         this.admirar() {
+
+            this.posicao += 1;
+            this.interaçoes 
+        
+         }
+
+
+
+    
+
+
+
+
+
+
+
+}
